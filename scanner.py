05afde123bc635cd004684cd97c08cea20208af8
amauvp <amauvp@Attacker.myguest.virtualbox.org>
2023-11-13 16:00:17 +0100

    Update scanner script

--- scanner.py
+++ scanner.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 
-dnsSource = "192.168.68.114"
+dnsSource = "192.168.68.109"
 dnsDestination = "192.168.68.53"
 queryTypes = ["ANY", "A", "AAAA", "CNAME", "MX", "NS", "PTR", "SOA", "TXT"]
 
@@ -11,25 +11,27 @@ with open("tld-list.txt", "r") as f:
     for line in tmp:
         queryNames.append(line.replace("\n", ""))
 
-packetNumer = 0
+packetNumber = 0
 for domain in queryNames:
     successes = []
 
     for queryType in queryTypes:
         packetNumber += 1
 
-        dnsQuery = IP(src=dnsSource, dst=dnsDestination) / UDP(sport=RandShort(), dport=53) / DNS(rd=1, do=0, qd=DNSQR(qname=domain, qtype=queryType), ar=DNSRROPT(rclass=8192))
+        dnsQuery = IP(src=dnsSource, dst=dnsDestination) / UDP(sport=RandShort(), dport=53) / DNS(rd=1, ad=0, cd=1, qd=DNSQR(qname=domain, qtype=queryType), ar=DNSRROPT(rclass=8192))
 
         try:
             response = sr1(dnsQuery, verbose=0, timeout=1)
-            print(response.summary())
+            response.show()
             
             # Check if the response was transmitted via TCP
-            if response[DNS].tc == 1:
+            if response[IP].proto == 6:
+                print("tcp")
                 successes.append(0)
-            else:
+            elif response[IP].proto == 17:
                 successes.append(1)
-        except:
+        except Exception as e:
+            print(f"An exception occurred: {e}\n")
             successes.append(0)
 
     # Check if the sum of successes is equal to the len of queryTypes
